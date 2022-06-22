5b1a7a9b7d0a26be4fa93eb737105c10b8ae2367
leonid-tkach <79228337+leonid-tkach@users.noreply.github.com>
2022-06-22 16:13:00 -0400

    10.5.3

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("c:\\users\\lt\\anaconda3\\envs\\pythondata\\lib\\site-packages")
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
--- scraping.py
+++ scraping.py
@@ -1,13 +1,29 @@
 import sys
 sys.path.append("c:\\users\\lt\\anaconda3\\envs\\pythondata\\lib\\site-packages")
-# Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+  executable_path = {'executable_path': ChromeDriverManager().install()}
+  browser = Browser('chrome', **executable_path, headless=False)
+
+  news_title, news_paragraph = mars_news(browser)
+
+  # Run all scraping functions and store results in dictionary
+  data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+  }
+
+  # Stop webdriver and return data
+  browser.quit()
+  return data
 
 def mars_news(browser):
 
@@ -72,4 +88,8 @@ def mars_facts():
 
   return df.to_html()
 
-browser.quit()
+if __name__ == "__main__":
+  # If running as script, print scraped data
+  print(scrape_all())
+
+  
